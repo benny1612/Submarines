d605d7ff67a18e2b55a6c3b1804912349c04818a
benny_neiman <bn161299@gmail.com>
2025-10-28 13:04:55 +0200

    matrix function

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 #
+
+#מקבל אינפוט לגודל המטריצה
+def input_matrix():
+    
+    matrix_size=int(input("enter size of matrix: "))
+    return matrix_size
+
+matrix_size=input_matrix()
+    
+    
+
+#יוצר את המטריצה לפי האינפוט שהתקבל
+    
+def creata_matrix(rows=int,coloms=None):
+    coloms=rows
+    matrix=[]
+    for i in range(rows):
+        row=[]
+        for j in range(coloms):
+            row.append(0*rows)
+        matrix.append(row)
+    return matrix 
+
+matrix=creata_matrix(matrix_size)
+
+
+#מדפיס את המטריצה בדו מימדי
+
+def matrix_printer(matrix):
+    for i in range(len (matrix)):
+        for j in range (len (matrix[i])):
+            print(matrix[i][j],end=" ")
+        print()
+
+matrix_printer(matrix)
+
+
+
